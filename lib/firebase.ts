c5c8640deb93529f9e05a6f1ba3691fba882c6a8
MohammedAl-Haifi <mohammed.al-haifi@sait.ca>
2025-11-06 15:53:57 -0700

    saving work

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,3 +10,8 @@ const firebaseConfig = {
     messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 }
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
+
+export const auth = getAuth(app)
+export const db = getFirestore
